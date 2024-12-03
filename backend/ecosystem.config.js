e341f0508a31af01a2aa7a7708964ac9e9b8e203
Alexander Chukharev <chooharev@gmail.com>
2024-12-03 18:41:16 +0300

    crash-test

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF,
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name   : "backend",
+    script : "./source/backend/src/app.ts"
+  }],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/choohio/nodejs-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build',
+    },
+  },
+}
