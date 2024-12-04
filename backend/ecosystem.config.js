92e559273462ccb46fe6c6425d019609fbbb0723
Alexander Chukharev <chooharev@gmail.com>
2024-12-04 11:38:51 +0300

    backend ecosystem.config.js

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,23 +1,24 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config({path: './.env.deploy'});
 
 const {
-  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF,
-} = process.env.deploy;
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPOSITORY
+} = process.env;
 
 module.exports = {
   apps: [{
     name   : "backend",
-    script : "./source/backend/src/app.ts"
+    script : "dist/app.js"
   }],
   deploy: {
     production: {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: 'https://github.com/choohio/nodejs-pm2-deploy.git',
+      repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'npm i && npm run build',
+      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      'post-deploy': 'cd backend && pwd && npm ci && npm run build pm2 startOrRestart ecosystem.config.js --env production',
     },
   },
 }
