704f8103ed8b963a9ec3cb287f8a88b5a936b39a
Alexander Chukharev <chooharev@gmail.com>
2024-12-05 14:26:37 +0300

    frontend deploy fix

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -6,10 +6,6 @@ const {
 } = process.env;
 
 module.exports = {
-  apps: [{
-    name   : "backend",
-    script : "dist/app.js"
-  }],
   deploy: {
     production: {
       user: DEPLOY_USER,
@@ -17,7 +13,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPOSITORY,
       path: DEPLOY_PATH,
-      'post-deploy': 'cd backend && pwd && npm ci && npm run build',
+      'post-deploy': 'cd frontend && pwd && npm ci && npm run build',
     },
   },
 }
